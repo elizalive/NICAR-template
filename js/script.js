2afda571230c6b708ab2940ef032e33c371fff53
pbustamante <peggybustamante@Peggys-MacBook-Air.local>
2015-03-01 22:34:32 -0800

    initial commit
    Template for NICAR intro to web coding workshop

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* script.js 
+   Author: Peggy Bustamante
+   Date: February 2015
+*/
+
+
+$(document).ready(function(){ // begin document.ready block
+
+	var total = 0;
+
+	// jQuery will go here	
+
+}); //end document.ready block
